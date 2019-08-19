32563037583f069cc36b7a13a4e8e07bdbb0d84a
Albérico de Castro <alberico.castro@gethelix.org>
2019-08-19 01:46:52 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script installs the requirements for the Helix installation.
+
+clear				# clear terminal window
+
+echo "Updating Ubuntu"
+echo
+
+sudo apt update
+sudo apt upgrade
+
+echo "Ubuntu is updated!"
