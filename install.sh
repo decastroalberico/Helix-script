d220bb5c94ca87f1c5e20f49159b3642c2ef4d79
Albérico de Castro <alberico.castro@gethelix.org>
2019-08-19 02:53:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,6 +63,7 @@ then
   git clone https://github.com/fabiocabrini/Helix_IoT_MQTT.git
   cd Helix_IoT_MQTT
   sudo docker-compose up -d
+  echo "MQTT"
 
 else
   echo "Installing Helix Sandbox with COaP"
@@ -71,6 +72,7 @@ else
   cd helix-sandbox/compose
   echo "put_here_your_encryption_key" > secrets/aes_key.txt
   sudo docker-compose up -d
+  echo "Coap"
 fi
 
 echo "Helix installed with success"
